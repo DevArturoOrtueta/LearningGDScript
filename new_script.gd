0f7b1115bf0a6ee1d850c0908f7e1b69edb491fc
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2024-04-21 22:16:44 -0500

    Vector3 and Color added

--- new_script.gd
+++ new_script.gd
@@ -15,3 +15,8 @@ var array = [1,2,3]
 
 
 var coordenadas = Vector2(1.1, 7.18) #Vecto2 nos permite a nosotros tomar las coordenadas de un objeto x y y
+var coordenadas_enteras = Vector2i(1,7) #Vector2i es lo mismo que el anterior, solo que este permite tomar solo enteros.
+
+var coordenadas3D = Vector3(1,1,3) #Vector3 permite tomar los ejes x, y y z, tiene su equivalente a enteros Vector3i
+
+var color_fondo = Color(1,1,1,0) #Color permite tomar un color rgba
