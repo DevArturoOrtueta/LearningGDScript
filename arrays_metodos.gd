17bd3f1b6e2c78fd71d68754d5219a53154a0f8f
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2024-04-22 09:47:50 -0500

    Array methods part 2

--- arrays_metodos.gd
+++ arrays_metodos.gd
@@ -9,12 +9,26 @@ func _ready():
 	# append incluye al final de la lista
 	inventario.append("hierbas")
 	
-	for posicion in inventario.size():
-		print(inventario[posicion])
-		if inventario[posicion] == "pocion":
-			inventario[posicion] = "pocion vacia"
-		elif inventario[posicion] == "hierbas":
-			inventario.remove_at(posicion)
+	var loot = ["pocion", "arma", "escudo"]
+	inventario.append_array(loot) # hace un insert de un arreglo al arreglo ya existente al final
+	
+	inventario.insert(2, "lanza") #inserta en la posicion que especificamos
+	
+	print(inventario.find("arma")) #Nos indica la posicion del elemento
+	
+	print(inventario.has("arma")) # Nos indica true si el elemento existe en el array
+	
+	loot.reverse() #Lo ordena en reversa
+	loot.sort() #Lo ordena ascendente
+	loot.shuffle() #Lo ordena aleatoriamente
+	#loot.clear() # Lo limpia completamente
+	print(loot)
+	#for posicion in inventario.size():
+	#	print(inventario[posicion])
+	#	if inventario[posicion] == "pocion":
+	#		inventario[posicion] = "pocion vacia"
+	#	elif inventario[posicion] == "hierbas":
+	#		inventario.remove_at(posicion)
 			# remove_at elimina el elemento de una posicion
 
 	print(inventario)
