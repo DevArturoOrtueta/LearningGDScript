a763c51a95ed9b49c8f081c3aadde186b5f03be7
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2024-04-21 22:13:38 -0500

    Initial commit

---
+++ new_script.gd
@@ -0,0 +1,17 @@
+extends Node
+
+func _ready():
+	var cazuela = "Cebolla"
+	print(cazuela)
+
+#Tipos de datos
+
+var cadena = "Cebolla y patatas"
+var numero_entero = 5
+var numero_real = 78.8
+var booleano = false
+var mi_variable = null
+var array = [1,2,3]
+
+
+var coordenadas = Vector2(1.1, 7.18) #Vecto2 nos permite a nosotros tomar las coordenadas de un objeto x y y
